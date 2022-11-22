e97f0b39eecd6f1a54386854ce027280943e1f51
Cezar Budeci <cezarbudeci@gmail.com>
2022-11-23 01:26:40 +0200

    Added exercises 6.3 - 6.21

---
+++ part6/exercises 6.3 - 6.21/src/components/Filter.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { filter } from "../reducers/filterReducer";
+
+const Filter = (props) => {
+    const handleInput = (e) => {
+        const value = e.target.value;
+        props.filter({ filter: value });
+    };
+
+    const style = {
+        marginBottom: 10
+    };
+
+    return (
+        <div style={style}>
+            filter <input onChange={handleInput} />
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = {
+    filter,
+};
+
+const ConnectedFilter = connect(mapStateToProps, mapDispatchToProps)(Filter);
+
+export default ConnectedFilter;
---
+++ part6/exercises 6.3 - 6.21/src/reducers/notificationReducer.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    message: '',
+    id: ''
+};
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState,
+    reducers: {
+        setNotification(state, action) {
+            return {
+                message: action.payload.message,
+                id: action.payload.id
+            };
+        },
+        removeNotification(state, action) {
+            return {
+                message: '',
+                id: ''
+            };
+        }
+    }
+});
+
+export const { setNotification, removeNotification, getNotification } = notificationSlice.actions;
+
+export const createNotification = (text, time) => {
+    return (dispatch, getState) => {
+        return new Promise(() => {
+            const timeInMillis = time * 1000;
+            const existingNotification = getState().notification;
+
+            if (existingNotification.id) {
+                clearTimeout(existingNotification.id);
+            }
+
+            const timeoutId = setTimeout(() => {
+                dispatch(removeNotification());
+            }, timeInMillis);
+
+            dispatch(setNotification({ message: text, id: timeoutId }));
+        });
+    };
+}
+
+export default notificationSlice.reducer;
